7ea1637d22de6b5a87178c24e3dfebc31243f300
p6remant <paudelremant6@gmail.com>
2026-01-21 10:10:16 +0545

    feat: create common component index for easier exports of UI elements

---
+++ src/components/common/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./Button";
+export type { ButtonProps } from "./Button";
+export { Input } from "./Input";
+export type { InputProps } from "./Input";
+export { Textarea } from "./Textarea";
+export type { TextareaProps } from "./Textarea";
+export { Alert } from "./Alert";
+export { Loader } from "./Loader";
+export { ErrorState } from "./ErrorState";
